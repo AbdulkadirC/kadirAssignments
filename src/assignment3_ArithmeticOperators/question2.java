b7ed22238d350cd8816289df1f210e28c7728a8d
Kadir <kadir91nl@gmail.com>
2021-06-29 11:32:01 +0200

    updated codes1

--- src/assignment3_ArithmeticOperators/question2.java
+++ src/assignment3_ArithmeticOperators/question2.java
@@ -5,11 +5,12 @@ public class question2 {
 	public static void main(String[] args) {
 		
 		int year, birthDay, age;
+		String name = "Ozzy";
 		birthDay = 1991;
 		year = 2021;
 		age = year - birthDay;
 		
-		System.out.println("Hello, Ozzy! Based on your input your age is " + age);
+		System.out.println("Hello, " + name + " Based on your input your age is " + age);
 		
 		
 		
